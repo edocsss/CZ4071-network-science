66adab480ca98d1488abe00ca401d0206cc00bc9
Edwin Candinegara <edwin.candinegara@gmail.com>
2017-02-07 01:16:34 +0800

    use graph_tool instead

--- util/data_util.py
+++ util/data_util.py
@@ -1,53 +1,40 @@
 import config as CONFIG
-import networkx as nx
+import graph_tool as gt
 import os
-import pickle
+import graph_tool.clustering as gt_clustering
+import graph_tool.stats as gt_stats
 
 
-def _read_network(file_name='google_network_raw', replace_pickle=False):
-    """
-    This method will cache the resulting Graph in the form of a pickle file.
-    The edges list file must be in a TXT file!!
-    :param file_name: the network's edges list TXT file
-    :return: graph representing the network
-    """
-
-    pickle_name = file_name + '.p'
-    pickle_path = os.path.join(CONFIG.DATA_DIR_PATH, pickle_name)
-
-    if os.path.isfile(pickle_path) and not replace_pickle:
-        return _read_pickle_by_path(pickle_path)
-
-    file_name_with_ext = file_name + '.txt'
-    file_path = os.path.join(CONFIG.DATA_DIR_PATH, file_name_with_ext)
-
-    G = nx.read_edgelist(file_path, create_using=nx.DiGraph())
-    _write_pickle_to_path(pickle_path, G)
+def _read_network(file_name='google_network_raw.csv'):
+    file_path = os.path.join(CONFIG.DATA_DIR_PATH, file_name)
+    f = open(file_path, 'r')
+    G = gt.load_graph_from_csv(file_name=f, directed=True, csv_options={'delimiter': '\t'})
+    f.close()
 
     return G
-
-
-def _read_pickle_by_path(pickle_path):
-    f = open(pickle_path, 'rb')
-    obj = pickle.load(f)
-    f.close()
-
-    return obj
-
-
-def _write_pickle_to_path(pickle_path, obj):
-    f = open(pickle_path, 'wb')
-    pickle.dump(obj, f)
-    f.close()
 
 
 def get_network():
     return _network
 
 
-_network = _read_network(file_name='google_network_raw', replace_pickle=False)
+_network = _read_network(file_name='google_network_raw.csv')
 
 
 if __name__ == '__main__':
     network = get_network()
-    print(network.edges())
+    print network.num_vertices()
+    print network.num_edges()
+
+    import time
+    start = time.time()
+    print gt_clustering.local_clustering(network, undirected=False)
+    print 'Time 1:', time.time() - start
+
+    start = time.time()
+    print gt_clustering.global_clustering(network)
+    print 'Time 2:', time.time() - start
+
+    start = time.time()
+    print gt_stats.distance_histogram(network)
+    print 'Time 3:', time.time() - start
