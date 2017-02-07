8c7701cb97d95bdef57f2d27df84ece3a9ed4aad
Edwin Candinegara <edwin.candinegara@gmail.com>
2017-02-07 13:30:35 +0800

    use graph tool

--- util/data_util.py
+++ util/data_util.py
@@ -3,15 +3,47 @@ import graph_tool as gt
 import os
 import graph_tool.clustering as gt_clustering
 import graph_tool.stats as gt_stats
+import cPickle
+import gc
 
 
-def _read_network(file_name='google_network_raw.csv'):
+def _read_network(file_name='google_network_raw.csv', replace_pickle=False):
     file_path = os.path.join(CONFIG.DATA_DIR_PATH, file_name)
+    pickle_name = file_name.split('.')[0] + '.p'
+    pickle_path = os.path.join(CONFIG.DATA_DIR_PATH, pickle_name)
+
+    if os.path.isfile(pickle_path) and not replace_pickle:
+        return _load_network()
+
     f = open(file_path, 'r')
     G = gt.load_graph_from_csv(file_name=f, directed=True, csv_options={'delimiter': '\t'})
     f.close()
 
+    _store_network(G)
     return G
+
+
+def _store_network(network, file_name='google_network_raw.p'):
+    file_path = os.path.join(CONFIG.DATA_DIR_PATH, file_name)
+    f = open(file_path, 'wb')
+
+    gc.disable()
+    cPickle.dump(network, f)
+    gc.enable()
+
+    f.close()
+
+
+def _load_network(file_name='google_network_raw.p'):
+    file_path = os.path.join(CONFIG.DATA_DIR_PATH, file_name)
+    f = open(file_path, 'wb')
+
+    gc.disable()
+    network = cPickle.load(f)
+    gc.enable()
+
+    f.close()
+    return network
 
 
 def get_network():
